763b0b72a65a5345005f2c8e4275c67f25e6c0f8
Ryn. <rynstwrt17@gmail.com>
2025-10-29 13:13:26 -0500

    Backup

--- FindBestDeals/src/main.jsx
+++ FindBestDeals/src/main.jsx
@@ -4,20 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
 import Home from "./Home.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <BrowserRouter>
-          {/* <App /> */}
-
           <Routes>
               <Route element={<App />}>
+                  <Route path={"*"} element={<NotFound />} />
                   <Route index element={<Home />} />
                   <Route path={"deals"} element={<App />} />
               </Route>
-
-              {/* <Route index element={<Home />} /> */}
-              {/* <Route path={"deals"} element={<App />} /> */}
           </Routes>
       </BrowserRouter>
   </StrictMode>
---
+++ FindBestDeals/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import "./NotFound.css";
+
+
+export default function NotFound() {
+    return (
+        <>
+            <h2>404</h2>
+            <h3>Not Found</h3>
+        </>
+    );
+}
