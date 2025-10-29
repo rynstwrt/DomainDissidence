9ebf784398d708ed6e896cdeb1cbdd9c64221810
Ryn. <rynstwrt17@gmail.com>
2025-10-29 13:08:00 -0500

    adsf

--- FindBestDeals/src/App.jsx
+++ FindBestDeals/src/App.jsx
@@ -3,6 +3,7 @@
 // import viteLogo from '/vite.svg'
 import Home from "./Home.jsx";
 import './App.css'
+import { Outlet } from "react-router";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
         {/* <Home /> */}
         <h1>asdf</h1>
+        <Outlet />
       {/* <div> */}
       {/*   <a href="https://vite.dev" target="_blank"> */}
       {/*     <img src={viteLogo} className="logo" alt="Vite logo" /> */}
--- FindBestDeals/src/main.jsx
+++ FindBestDeals/src/main.jsx
@@ -11,8 +11,13 @@ createRoot(document.getElementById('root')).render(
           {/* <App /> */}
 
           <Routes>
-              <Route index element={<Home />} />
-              <Route path={"deals"} element={<App />} />
+              <Route element={<App />}>
+                  <Route index element={<Home />} />
+                  <Route path={"deals"} element={<App />} />
+              </Route>
+
+              {/* <Route index element={<Home />} /> */}
+              {/* <Route path={"deals"} element={<App />} /> */}
           </Routes>
       </BrowserRouter>
   </StrictMode>
