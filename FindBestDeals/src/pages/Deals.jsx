9cc971bf072107c2ea503ab592a273c9449b9a39
Ryn. <rynstwrt17@gmail.com>
2025-10-30 16:29:49 -0500

    Start adding styles

--- FindBestDeals/src/pages/Deals.jsx
+++ FindBestDeals/src/pages/Deals.jsx
@@ -3,8 +3,8 @@ import './Deals.css'
 
 export default function Deals() {
   return (
-    <>
+    <main>
         <h1>Deals</h1>
-    </>
+    </main>
   )
 }
--- FindBestDeals/src/pages/NotFound.jsx
+++ FindBestDeals/src/pages/NotFound.jsx
@@ -3,9 +3,9 @@ import "./NotFound.css";
 
 export default function NotFound() {
     return (
-        <>
+        <main>
             <h2>404</h2>
             <h3>Not Found</h3>
-        </>
+        </main>
     );
 }
