e9ab5689d1364c6371e9d7e912d7a35c0a49194c
Ryn. <rynstwrt17@gmail.com>
2025-10-30 17:56:17 -0500

    Start home page styling

--- FindBestDeals/src/pages/Home.jsx
+++ FindBestDeals/src/pages/Home.jsx
@@ -4,7 +4,18 @@ import "./Home.css";
 export default function Home() {
     return (
         <main>
-            <h1>HOME</h1>
+            <div id={"background"}></div>
+
+            <div id={"landing-content"}>
+                <div id={"landing-text"}>
+                    <h1>FindBestDeals</h1>
+                    <p>Find the best deals for your favorite brands!</p>
+                </div>
+
+                <div id={"landing-buttons"}>
+                    <button>Check Deals</button>
+                </div>
+            </div>
         </main>
     );
 }
